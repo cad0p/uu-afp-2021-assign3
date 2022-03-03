8cc1d4253a1c2d22cfc6381d493b97c8d495de18
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-03-03 19:03:20 +0100

    managed to show the vector

--- assign3-cad0p/src/Assign3/Vector.hs
+++ assign3-cad0p/src/Assign3/Vector.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE KindSignatures #-}
 {-|
 Module      : Assign3.Vector
 Description : Term-level fixpoints
@@ -10,23 +12,36 @@ Stability   : experimental
 
 module Assign3.Vector (Vec) where
 
-data Zero
-data Succ n
+data Nat
+  = Zero
+  | Succ Nat
 
-data Vec a n where
-  Nil :: Vec a Zero
-  Cons :: a -> Vec a n -> Vec a (Succ n)
+data Vec a (n :: Nat) where
+  Nil :: Vec a 'Zero
+  Cons :: a -> Vec a n -> Vec a ('Succ n)
+
+instance Eq a => Eq (Vec a n) where
+  Nil == Nil             = True
+  Cons a v == Cons a' v' = a == a' && v == v'
+
+instance Show a => Show (Vec a n) where
+  show v = '[' : show' v ++ "]" where
+    show' :: Show a => Vec a n -> String
+    show' Nil          = ""
+    show' (Cons a Nil) = show a
+    show' (Cons a v)   = show a ++ ", " ++ show' v
 
 
-data Foo a
-  = Foo
-      { a  :: Int
-      , a2 :: String
-        -- ^ some haddock
-      }
-  | Bar
-      { b :: a
-      }
-  deriving (Eq, Show)
+
+class Vector v where
+  toList :: v a n -> [a]
+  fromList :: [a] -> v a n
+
+{-| 'Vec' is a 'Vector' with a fixed number of elements
+-}
+-- instance Vector Vec where
+--   toList :: Vec a n -> [a]
+--   toList Nil         = []
+--   toList (Cons x xs) = x : toList xs
 
 
