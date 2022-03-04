41c2d8678a222c377d71a8ee89482381aeb467c1
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-03-04 03:56:44 +0100

    2.1

--- assign3-cad0p/src/Assign3/Nested.hs
+++ assign3-cad0p/src/Assign3/Nested.hs
@@ -32,12 +32,29 @@ square1 = Succ (Zero (Cons (1 `Cons` Nil) Nil))
   [[1, 0]
   ,[0, 1]]
 -}
--- square2 :: Square Int
--- square2 = Succ (Succ ())
+square2 :: Square Int
+square2 = Succ (Succ (Zero (
+      1 `Cons` (0 `Cons` Nil) `Cons` (
+      0 `Cons` (1 `Cons` Nil) `Cons` Nil))))
+
+{-|
+  [[1, 2, 3],
+   [4, 5, 6]
+  ,[7, 8, 9]]
+-}
+square3 :: Square Int
+square3 = Succ( Succ (Succ (Zero (
+      1 `Cons` (2 `Cons` ( 3 `Cons` Nil)) `Cons` (
+      4 `Cons` (5 `Cons` ( 6 `Cons` Nil)) `Cons` (
+      7 `Cons` (8 `Cons` ( 9 `Cons` Nil)) `Cons` Nil))))))
 
 
 {-|
   from here: https://joelburget.com/data-newtype-instance-class/
 -}
--- class Shape a where
---   fromList2 :: [[a]]
+class Shape a where
+  fromList2 :: [[b]] ->   a
+  toList2   ::   a   -> [[b]]
+
+-- instance Shape (Square a) where
+--   toList2
