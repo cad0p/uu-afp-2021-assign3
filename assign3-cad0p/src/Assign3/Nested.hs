0e5d876da4f71a7b3317fecaae0068dd3a8ffc16
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-03-04 03:09:47 +0100

    this is madness, giving up showing the square

--- assign3-cad0p/src/Assign3/Nested.hs
+++ assign3-cad0p/src/Assign3/Nested.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-|
 Module      : Assign3.Nested
 Description : Term-level fixpoints
@@ -17,6 +19,29 @@ data Square' t a
 data Nil a = Nil
 data Cons t a
   = Cons a (t a)
+
+instance Show a => Show (Nil a) where
+  show a = ""
+
+instance (Show a) => Show (Cons Nil a) where
+  show (Cons a Nil) = show a
+
+instance (Show a, Show (Square a)) => Show (Cons Square a) where
+  show (Cons a (Zero Nil))                = show a ++ "1\n"
+  show (Cons a (Succ(Zero (Cons b Nil)))) = show a ++ "2\n"
+  show (Cons a (Succ(Succ b)))            = show a ++ "3\n"
+
+
+instance Show a => (Show (Square' (Cons Nil) a)) where
+  show (Zero (Cons x xs))  = show x
+  show (Succ (Zero (Nil))) = ""
+
+
+instance Show a => Show (Square a) where
+  show (Zero Nil) = ""
+  show (Succ a)   = show a ++ "\n"
+
+
 
 {-|
   from here: https://joelburget.com/data-newtype-instance-class/
